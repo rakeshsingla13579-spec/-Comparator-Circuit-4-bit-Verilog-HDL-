34b7b7522ec190b7cbc0332107596a958df496c7
Rakesh Singla <rakeshsingla13579@gmail.com>
2025-08-30 21:04:25 +0530

    verilog.v

---
+++ verilog.v
@@ -0,0 +1,29 @@
+// Code your design here
+// comparator_4bit.v
+module comparator_4bit (
+    input  [3:0] A,
+    input  [3:0] B,
+    output reg A_gt_B,
+    output reg A_lt_B,
+    output reg A_eq_B
+);
+
+always @(*) begin
+    if (A > B) begin
+        A_gt_B = 1;
+        A_lt_B = 0;
+        A_eq_B = 0;
+    end
+    else if (A < B) begin
+        A_gt_B = 0;
+        A_lt_B = 1;
+        A_eq_B = 0;
+    end
+    else begin
+        A_gt_B = 0;
+        A_lt_B = 0;
+        A_eq_B = 1;
+    end
+end
+
+endmodule
